89faf03ac832b6c19475d02a5865f9497240fe09
Amarylian <lucja.m.lucy@gmail.com>
2017-09-26 22:07:18 +0200

    Added database model

---
+++ Database/SQLScripts/select_all_elements.sql
@@ -0,0 +1,5 @@
+select * from elements.text;
+select * from elements.translation;
+select * from elements.text natural join elements.translation;
+select * from elements.sound;
+select * from elements.picture;
---
+++ Database/SQLScripts/select_all_types.sql
@@ -0,0 +1,7 @@
+select * from types.language;
+select * from types.role;
+select * from types.userStatus;
+select * from types.align;
+select * from types.game;
+select * from types.category;
+select * from types.varibleType;
